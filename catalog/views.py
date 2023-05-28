9ad4c86c88c43bd1055024d68dbe7b3d1cdac8c2
Olga Yarosh <O_ll_y_a@mail.ru>
2023-05-28 22:21:10 +0300

    homework2

--- catalog/views.py
+++ catalog/views.py
@@ -5,3 +5,12 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'catalog/index.html')
+
+
+def contact(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        print(name, email, message)
+    return render(request, 'catalog/contact.html')
--- config/urls.py
+++ config/urls.py
@@ -17,7 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from catalog.views import index, contact
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('catalog.urls'))
+    # path('/', index),
+    # path('/contact', contact)
 ]
