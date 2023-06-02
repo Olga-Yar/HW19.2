269dac9316ba8d29679214080c5c98097ec01994
Olga Yarosh <O_ll_y_a@mail.ru>
2023-06-02 19:22:33 +0300

    создание моделей продукт и категория

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=150, verbose_name='наименование')
+    about = models.CharField(max_length=150, verbose_name='описание')
+    image = models.ImageField(upload_to='products/', verbose_name='превью')
+    category = models.CharField(max_length=150, verbose_name='категория')
+    price_lot = models.IntegerField(verbose_name='цена за покупку')
+    date_create = models.DateTimeField(verbose_name='дата создания')
+    date_last_change = models.DateTimeField(verbose_name='дата последнего изменения')
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=150, verbose_name='наименование')
+    about = models.CharField(max_length=150, verbose_name='описание')
