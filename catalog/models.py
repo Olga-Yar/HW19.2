6cc090432c55c8c2ab2e166421d12e4ffb491f76
Olga Yarosh <O_ll_y_a@mail.ru>
2023-06-03 21:36:05 +0300

    добавление created_at в класс Category

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.db import models
 
 # Create your models here.
@@ -20,6 +22,7 @@ class Product(models.Model):
 class Category(models.Model):
     name = models.CharField(max_length=150, verbose_name='наименование')
     about = models.CharField(max_length=150, verbose_name='описание')
+    created_at = models.DateTimeField(verbose_name='дата создания', default='2023-06-03')
 
     def __str__(self):
         return f'{self.name}: {self.about}'
