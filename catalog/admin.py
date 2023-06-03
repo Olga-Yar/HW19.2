69051bb767c95e6bbacbc60796bde59727d08690
Olga Yarosh <O_ll_y_a@mail.ru>
2023-06-03 22:01:26 +0300

    создание админки

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
+from catalog.models import Product, Category
+
+
 # Register your models here.
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price_lot', 'category')
+    list_filter = ('category',)
+    search_fields = ('name', 'about',)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_filter = ('category',)
+    search_fields = ('name', 'about',)
