3ebd273c5e16e4ee828c2fe9269c648b9fc0f735
Olga Yarosh <O_ll_y_a@mail.ru>
2023-06-04 19:44:43 +0300

    изменение модели

--- catalog/models.py
+++ catalog/models.py
@@ -7,21 +7,21 @@ NULLABLE = {'blank': True, 'null': True}
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=150, verbose_name='наименование')
-    about = models.CharField(max_length=150, verbose_name='описание')
+    name = models.CharField(max_length=300, verbose_name='наименование')
+    about = models.TextField(blank=True, verbose_name='описание')
     image = models.ImageField(upload_to='products/', verbose_name='превью')
-    category = models.CharField(max_length=150, verbose_name='категория')
+    category = models.CharField(max_length=300, verbose_name='категория')
     price_lot = models.IntegerField(verbose_name='цена за покупку')
-    date_create = models.DateTimeField(verbose_name='дата создания')
-    date_last_change = models.DateTimeField(verbose_name='дата последнего изменения')
+    date_create = models.DateTimeField(auto_now_add=True, verbose_name='дата создания')
+    date_last_change = models.DateTimeField(auto_now=True, verbose_name='дата последнего изменения')
 
     def __str__(self):
         return f'{self.name}: {self.about}'
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=150, verbose_name='наименование')
-    about = models.CharField(max_length=150, verbose_name='описание')
+    name = models.CharField(max_length=300, verbose_name='наименование')
+    about = models.TextField(blank=True, verbose_name='описание')
 
     def __str__(self):
         return f'{self.name}: {self.about}'
